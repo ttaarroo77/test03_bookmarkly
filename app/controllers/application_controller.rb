4ea836a2553f98dd625d61074a0da67eea59f4bc
ttaarroo77 <nakazawatarou@gmail.com>
2025-02-21 10:28:58 +0900

    認証機能の改善

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery unless: -> { request.format.json? }
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  respond_to :html, :json
+
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # nameのバリデーションを任意に変更
+  validates :name, presence: false  # または完全に削除
+
+  validates :email, presence: true, uniqueness: true
+
+  # nameを許可する属性として追加
+  attr_accessor :name
+
   has_many :bookmarks, dependent: :destroy
 end
